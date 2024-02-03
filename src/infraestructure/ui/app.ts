be10d1c1c7370c60515cad44b5a9fb09bbdff477
maxi-camonapp <maximiliano@camonapp.com>
2024-02-03 03:54:40 -0300

    test: add debug test usecases

--- src/infraestructure/ui/app.ts
+++ src/infraestructure/ui/app.ts
@@ -1,26 +1,48 @@
-// import { prompt } from 'enquirer'
-
-import { CreateTaskUsecase } from "../../application/usecases/create-task.usecase"
+import { CreateTaskUsecase, GetAllTasksUsecase, GetTaskByIdUsecase } from "../../application/usecases"
 import { TaskSqliteDAO } from "../daos/task-sqlite.dao"
 import { UIDGeneratorImplementation } from "../utils/uuidv4"
 
 (async () => {
-    // const response = await prompt({
-    //     type: 'input',
-    //     name: 'username',
-    //     message: 'What is your username?'
-    // });
-
+    
     const taskCreator = new CreateTaskUsecase(new TaskSqliteDAO(), new UIDGeneratorImplementation())
 
     const newTask = await taskCreator.execute({
-        title: 'tarea 1',
-        description: 'hacer la tarea',
+        title: 'tarea 3',
+        description: 'hacer la tarea sin dormirse',
+        endDate: new Date(),
+        startDate: new Date(),
+        projectId: "",
+    })
+    
+    /* const taskCreator = new CreateTaskUsecase(new TaskSqliteDAO(), new UIDGeneratorImplementation())
+
+    const newTask = await taskCreator.execute({
+        title: 'tarea 3',
+        description: 'hacer la tarea sin dormirse',
         endDate: new Date(),
         startDate: new Date(),
         projectId: "",
     })
 
     console.log('SUCCESS!!!',newTask)
+
+
+    const tasksGetter = new GetAllTasksUsecase(new TaskSqliteDAO())
     
+    const allTasks = await tasksGetter.execute()
+
+    console.log('todas las tasks', allTasks) 
+
+    const taskGetterById = new GetTaskByIdUsecase(new TaskSqliteDAO())
+
+    const taskFound = await taskGetterById.execute('db5493a3-0d6a-497f-9ba8-3a9a6604e1ea')
+
+    console.log("encontre", taskFound)¨*/
+
+    const tasksGetter = new GetAllTasksUsecase(new TaskSqliteDAO())
+    
+    const allTasks = await tasksGetter.execute()
+
+    console.log("all tasks", allTasks)
+
 })()
